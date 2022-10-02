2eb731db8a8c124d7322fa5b73fb1a7ab8765d08
Kiadtisak Lowongsa <kanlowongsa20@gmail.com>
2022-10-02 14:48:28 +0700

    created login state

--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -9,15 +9,20 @@ import {
   TextField,
   Theme,
   Typography,
+  Alert
 } from "@mui/material";
 import { Stack, SxProps } from "@mui/system";
 import { User } from "../../../types/user.type";
-
+import { useDispatch, useSelector } from "react-redux";
+import { RootReducers } from "../../../reducers";
+import * as loginActions from '../../../actions/login.action'
 type LoginPageProps = {
   //
 };
 
 const LoginPage: React.FC<any> = () => {
+  const loginReducer = useSelector((state: RootReducers) => state.loginReducer);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const classes: SxProps<Theme> | any = {
     root: { display: "flex", justifyContent: "center" },
@@ -59,6 +64,10 @@ const LoginPage: React.FC<any> = () => {
           value={values.password}
         />
         <br />
+
+        {loginReducer.isError && (
+          <Alert severity="error">Login failed.</Alert>
+        )}
         <Stack direction="row" spacing={2} sx={classes.buttons}>
           <Button
             onClick={() => navigate("/register")}
@@ -75,7 +84,7 @@ const LoginPage: React.FC<any> = () => {
             fullWidth
             variant="contained"
             color="primary"
-            disabled={isSubmitting}
+            disabled={loginReducer.isFetching}
           >
             Login
           </Button>
@@ -95,9 +104,8 @@ const LoginPage: React.FC<any> = () => {
               Login
             </Typography>
             <Formik
-              onSubmit={(values, { setSubmitting }) => {
-                alert(JSON.stringify(values));
-                setTimeout(() => setSubmitting(false), 1000);
+              onSubmit={(values) => {
+                dispatch(loginActions.login(values, navigate))
               }}
               initialValues={initialUser}
             >
